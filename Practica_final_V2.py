80e19b29babad1d55b6528b0692953d9478477bf
Carlos Damota <cdamota@msn.com>
2024-05-15 07:34:53 +0200

    definicion de metodosa v2

---
+++ Practica_final_V2.py
@@ -0,0 +1,27 @@
+from colorama import init, Fore, Style
+
+# Inicializa colorama
+init(autoreset=True)
+
+class ToDoList:
+    contador_id = 0
+    def __init__(self, titulo, descripcion="", estado="Pendiente"):
+        ToDoList.contador_id += 1
+        self.id = ToDoList.contador_id
+        self.titulo = titulo
+        self.descipcion = descripcion
+        self.estado = estado
+
+class ListaTareas:
+    def __init__(self):
+        self.tareas_pendientes = []
+
+    def añadir_tarea(self, titulo, descripcion="", estado="Pendiente"):
+        tareas = ToDoList(titulo, descripcion, estado)
+        self.tareas_pendientes.append(tareas)
+    
+    def mostrar_tareas(self):
+        for tareas in enumerate(self.tareas_pendientes):
+            if tareas.estado == "Pendiente":
+                print(Fore.RED + f"id" {tareas.id}, Titulo: {tareas.titulo}, Estado: {tareas.estado})
+            elif 
