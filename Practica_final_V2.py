d8977ad482bcb246f0c895274f0441c05aa31006
Carlos Damota <cdamota@msn.com>
2024-05-15 22:18:10 +0200

    definiendo main

--- Practica_final_V2.py
+++ Practica_final_V2.py
@@ -21,7 +21,33 @@ class ListaTareas:
         self.tareas_pendientes.append(tareas)
     
     def mostrar_tareas(self):
-        for tareas in enumerate(self.tareas_pendientes):
+        print("Lista de tareas")
+        for tareas in self.tareas_pendientes:
             if tareas.estado == "Pendiente":
-                print(Fore.RED + f"id" {tareas.id}, Titulo: {tareas.titulo}, Estado: {tareas.estado})
-            elif 
+                print(Fore.RED + f"id {tareas.id}, Titulo: {tareas.titulo}, Estado: {tareas.estado}")
+            elif tareas.estado == "En proceso":
+                print(Fore.YELLOW + f"id {tareas.id}, Titulo: {tareas.titulo}, Estado: {tareas.estado}")
+            elif tareas.estado == "Completada":
+                print(Fore.GREEN + f"id {tareas.id}, Titulo: {tareas.titulo}, Estado: {tareas.estado}")
+    
+    def empezar_tarea(self, id):
+        try:
+            tareas =self.tareas_pendientes[id]
+            tareas.estado = "En proceso"
+            print(f"Tarea '{tareas.titulo}' Marcada en proceso correctamente.")
+        except IndexError:
+            print('posicion ingresada no valida')
+
+def main():
+    listatareas = ListaTareas()
+
+    if ToDoList.contador_id > 0:
+        listatareas.mostrar_tareas()
+    
+    else:
+        print('No hay tareas')
+
+    
+    
+if __name__ == "__main__":
+    main()
