b5cbcdd836f907b572fd5d996310f898175ef542
Carlos Damota <cdamota@msn.com>
2024-05-16 15:55:52 +0200

    Integrando opciones tareas

--- Practica_final_V2.py
+++ Practica_final_V2.py
@@ -11,10 +11,26 @@ class ToDoList:
         self.titulo = titulo
         self.descipcion = descripcion
         self.estado = estado
+    
+    def mostrar_linea_separadora(caracter="-", longitud=50):
+        print("")
+        print(caracter * longitud)
+        print("")
+
+    def menu_opcciones():
+        ToDoList.mostrar_linea_separadora()
+        print("====== Menú Principal ======")
+        print("\n1. Añadir nueva tarea")
+        print("2. Mostrar tarea")
+        print("3. Marcar una tarea completada")
+        print("4. Eliminar una tarea")
+        print("5. Salir")
+        #print("6. Manual de intrucciones")
+        ToDoList.mostrar_linea_separadora()
 
 class ListaTareas:
     def __init__(self):
-        self.tareas_pendientes = []
+        ToDoList.tareas_pendientes = []
 
     def añadir_tarea(self, titulo, descripcion="", estado="Pendiente"):
         tareas = ToDoList(titulo, descripcion, estado)
@@ -32,11 +48,52 @@ class ListaTareas:
     
     def empezar_tarea(self, id):
         try:
-            tareas =self.tareas_pendientes[id]
+            tareas = self.tareas_pendientes[id]
             tareas.estado = "En proceso"
             print(f"Tarea '{tareas.titulo}' Marcada en proceso correctamente.")
         except IndexError:
             print('posicion ingresada no valida')
+    def completar_tarea(self, id):
+        try:
+            tarea = self.tareas_pendientes[id-1]
+            tarea.estado = "Completada"
+            print(f"Tarea '{tarea.titulo}' completada correctamente.")
+        except IndexError:
+            print('Posición ingresada no válida')
+
+    def ver_tarea(self, id):
+        try:
+            tarea = self.tareas_pendientes[id-1]
+            print(f"Tarea {tarea.id}:")
+            print(f"Título: {tarea.titulo}")
+            print(f"Descripción: {tarea.descripcion}")
+            print(f"Estado: {tarea.estado}")
+            print("E: Empezar, C: Completar, D: Borrar, V: Volver")
+        except IndexError:
+            print('Posición ingresada no válida')
+
+    def opcion_tarea(self, id):
+        while True:
+            try:
+                opcionesTarea = input("\nSeleccione una opción: ").capitalize # Solicita una opcion
+                if opcionesTarea == "V":
+                    break
+                elif opcionesTarea == "D":
+                    del self.tareas_pendientes[id-1]
+                    break
+                elif opcionesTarea == "E":
+                    tareas = self.tareas_pendientes[id]
+                    tareas.estado = "En proceso"
+                elif opcionesTarea == "c":
+                    tareas = self.tareas_pendientes[id]
+                    tareas.estado = "Completada"
+                elif opcionesTarea not in ['1', '2', '3', '4', '5']: # Verifica si la opción ingresada no está en la lista de opciones válidas
+                    raise ValueError("Opción no válida. Por favor, ingrese un número del 0 al 5.") # Genera una excepción ValueError si la opción no es válida
+                else:
+                    print("La opción seleccionada no está disponible en el manual. Por favor, seleccione otra opción.")
+
+            except IndexError:
+                print("opcion introducida no valida")
 
 def main():
     listatareas = ListaTareas()
@@ -47,7 +104,10 @@ def main():
     else:
         print('No hay tareas')
 
+    while True:
+        ToDoList.menu_opcciones()
+
     
-    
+
 if __name__ == "__main__":
     main()
